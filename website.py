a5e0d212842a5d850b735878ce125f71c7a2d49c
Zac de Moor <zacdemoor@Zacs-MacBook-Air.local>
2026-04-10 11:11:15 -0700

    Add photo and fix em dashes

--- website.py
+++ website.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="site_templates")
+app = Flask(__name__, template_folder="site_templates", static_folder="static")
 
 @app.route("/")
 def story():
