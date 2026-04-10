261f9518eeb059fb315730bc328d0f7dd57c33fb
Zac de Moor <zacdemoor@Zacs-MacBook-Air.local>
2026-04-10 11:18:54 -0700

    Use absolute paths for template and static folders

--- website.py
+++ website.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="site_templates", static_folder="static")
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__,
+            template_folder=os.path.join(base_dir, "site_templates"),
+            static_folder=os.path.join(base_dir, "static"))
 
 @app.route("/")
 def story():
